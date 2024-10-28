68012e635c4abd7ef75846ba6b70bb1dfc6aad87
Artyom <bol.art.2005@gmail.com>
2024-10-28 22:13:57 +0500

    first commit

--- main.py
+++ main.py
@@ -1,188 +1,59 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-First, a few callback functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Example of a bot-user conversation using ConversationHandler.
-Send /start to initiate the conversation.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import os
-import logging
-from typing import Dict
-
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, BotCommand
+import clck
+from telegram import Update, BotCommand
 from telegram.ext import (
     Application,
     CommandHandler,
-    ContextTypes,
-    ConversationHandler,
     MessageHandler,
-    PicklePersistence,
-    filters,
+    ContextTypes,
+    filters
 )
 
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-# set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Приветствие при запуске бота"""
+    await update.message.reply_text("Привет! Отправь мне ссылку, и я сокращу её.")
 
-logger = logging.getLogger(__name__)
+async def shorten_link(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Сокращает ссылку и отправляет пользователю."""
+    original_url = update.message.text.strip()
 
-CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+    if not original_url.startswith("http"):
+        await update.message.reply_text("Пожалуйста, отправьте правильный URL, начинающийся с http или https.")
+        return
 
-reply_keyboard = [
-    ["Age", "Favourite colour"],
-    ["Number of siblings", "Something else..."],
-    ["Done"],
-]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-
-
-def facts_to_str(user_data: Dict[str, str]) -> str:
-    """Helper function for formatting the gathered user info."""
-    facts = [f"{key} - {value}" for key, value in user_data.items()]
-    return "\n".join(facts).join(["\n", "\n"])
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Start the conversation, display any stored data and ask user for input."""
-    user = update.message.from_user
-    logger.info(f"User {user.full_name} started app.")
-    
-    reply_text = "Hi! My name is Doctor Botter."
-    if context.user_data:
-        reply_text += (
-            f" You already told me your {', '.join(context.user_data.keys())}. Why don't you "
-            "tell me something more about yourself? Or change anything I already know."
-        )
-    else:
-        reply_text += (
-            " I will hold a more complex conversation with you. Why don't you tell me "
-            "something about yourself?"
-        )
-    await update.message.reply_text(reply_text, reply_markup=markup)
-
-    return CHOOSING
-
-
-async def regular_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Ask the user for info about the selected predefined choice."""
-    text = update.message.text.lower()
-    context.user_data["choice"] = text
-    if context.user_data.get(text):
-        reply_text = (
-            f"Your {text}? I already know the following about that: {context.user_data[text]}"
-        )
-    else:
-        reply_text = f"Your {text}? Yes, I would love to hear about that!"
-    await update.message.reply_text(reply_text)
-
-    return TYPING_REPLY
-
-
-async def custom_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Ask the user for a description of a custom category."""
-    await update.message.reply_text(
-        'Alright, please send me the category first, for example "Most impressive skill"'
-    )
-
-    return TYPING_CHOICE
-
-
-async def received_information(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Store info provided by user and ask for the next category."""
-    text = update.message.text
-    category = context.user_data["choice"]
-    context.user_data[category] = text.lower()
-    del context.user_data["choice"]
-
-    await update.message.reply_text(
-        "Neat! Just so you know, this is what you already told me:"
-        f"{facts_to_str(context.user_data)}"
-        "You can tell me more, or change your opinion on something.",
-        reply_markup=markup,
-    )
-
-    return CHOOSING
-
-
-async def show_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Display the gathered info."""
-    await update.message.reply_text(
-        f"This is what you already told me: {facts_to_str(context.user_data)}"
-    )
-
-
-async def done(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Display the gathered info and end the conversation."""
-    if "choice" in context.user_data:
-        del context.user_data["choice"]
-
-    await update.message.reply_text(
-        f"I learned these facts about you: {facts_to_str(context.user_data)}Until next time!",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-    return ConversationHandler.END
+    try:
+        short_url = clck.ru(original_url)
+        if short_url:
+            await update.message.reply_text(f"Сокращенная ссылка: {short_url}")
+        else:
+            await update.message.reply_text("Не удалось сократить ссылку. Попробуйте позже.")
+    except Exception as e:
+        # Отправляем сообщение об ошибке пользователю и выводим её в лог
+        await update.message.reply_text("Произошла ошибка при сокращении ссылки. Попробуйте позже.")
+        print(f"Ошибка при сокращении ссылки: {e}")
 
 # Функция для регистрации команд в BotFather
 async def post_init(application: Application) -> None:
     bot_commands = [
         BotCommand("start", "Начало работы с ботом"),
-        # BotCommand("cancel", "Отменить текущую операцию")
     ]
     await application.bot.set_my_commands(bot_commands)
 
 def main() -> None:
-    """Run the bot."""
-    # Create the Application and pass it your bot's token.
-    persistence = PicklePersistence(filepath="data/data", single_file=False)
-    application = Application.builder().token(os.getenv("BOT_TOKEN")).persistence(persistence).post_init(post_init).build()
+    """Запуск бота"""
+    bot_token = os.getenv("BOT_TOKEN")
+    if not bot_token:
+        raise ValueError("Токен бота не задан. Укажите BOT_TOKEN в переменных окружения.")
 
-    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
-        states={
-            CHOOSING: [
-                MessageHandler(
-                    filters.Regex("^(Age|Favourite colour|Number of siblings)$"), regular_choice
-                ),
-                MessageHandler(filters.Regex("^Something else...$"), custom_choice),
-            ],
-            TYPING_CHOICE: [
-                MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Done$")), regular_choice
-                )
-            ],
-            TYPING_REPLY: [
-                MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Done$")),
-                    received_information,
-                )
-            ],
-        },
-        fallbacks=[MessageHandler(filters.Regex("^Done$"), done)],
-        name="my_conversation",
-        persistent=True,
-    )
+    # создаем приложение 
+    application = Application.builder().token(bot_token).post_init(post_init).build()
 
-    application.add_handler(conv_handler)
+    # добавляем обработчики команд и сообщений
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, shorten_link))
 
-    show_data_handler = CommandHandler("show_data", show_data)
-    application.add_handler(show_data_handler)
-
-    # Run the bot until the user presses Ctrl-C
+    # Запускаем бота до нажатия Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-
 
 if __name__ == "__main__":
     main()
